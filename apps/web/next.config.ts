8dade38458a3c53ba6511b312eef0e0dabd4039f
Favour Akande <favourakande1@gmail.com>
2026-03-18 02:06:29 +0100

    Configured next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = withPWA({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+})({
+  // TODO: Next.js configuration options will be added here
+})
 
 export default nextConfig;
