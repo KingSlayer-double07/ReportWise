d93bc26945e364cf79824d908ec018bcffb72cad
Favour Akande <favourakande1@gmail.com>
2026-05-03 13:33:06 +0100

    Integrated SchoolConfig and AcademicSession modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,10 +5,18 @@ import { TenantMiddleware } from './tenant/tenant.middleware.js';
 import { PrismaModule } from './common/prisma.module.js';
 import { AuthModule } from './auth/auth.module.js';
 import { SuperAdminModule } from './super-admin/super-admin.module.js';
+import { SchoolConfigModule } from './school-config/school-config.module.js';
+import { AcademicSessionModule } from './academic-session/academic-session.module.js';
 
 @Module({
   controllers: [AppController],
-  imports: [PrismaModule, TenantModule, AuthModule, SuperAdminModule]
+  imports: [
+    PrismaModule,
+    TenantModule,
+    AuthModule,
+    SuperAdminModule,
+    SchoolConfigModule,
+    AcademicSessionModule]
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
