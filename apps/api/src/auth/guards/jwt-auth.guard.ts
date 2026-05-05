1efc22201a7a99db30852aebf66f634b127ad9be
Favour Akande <favourakande1@gmail.com>
2026-05-05 16:16:35 +0100

    Update JwtAuthGuard to read the isPublic metadata and skip if true

--- apps/api/src/auth/guards/jwt-auth.guard.ts
+++ apps/api/src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,24 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
+import { IS_PUBLIC_KEY } from "../../common/public.decorator.js";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') implements CanActivate {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context) as boolean;
+  }
+}
