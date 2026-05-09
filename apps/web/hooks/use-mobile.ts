838c7e4fdfc82803f986a98be883ba16e765808e
Favour Akande <favourakande1@gmail.com>
2026-05-09 13:05:27 +0100

    Fixed nested hooks | Moved setState out of useEffect to avoid rendering issues

--- apps/web/hooks/use-mobile.ts
+++ apps/web/hooks/use-mobile.ts
@@ -2,18 +2,21 @@ import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
+const getIsMobile = () =>
+  typeof window !== "undefined" ? window.innerWidth < MOBILE_BREAKPOINT : undefined
+
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(getIsMobile)
+
+  const onChange = React.useCallback(() => {
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+  }, [])
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
     mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     return () => mql.removeEventListener("change", onChange)
-  }, [])
+  }, [onChange])
 
   return !!isMobile
 }
